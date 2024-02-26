990f39ba8ef74d84500cc420da45c94cb4c18a45
souvikkoley9674 <113795422+souvikkoley9674@users.noreply.github.com>
2024-02-26 19:17:56 +0530

    Login Error Fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -59,12 +59,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
     const tour = session.client_reference_id;
     const user = (await User.findOne({ email: session.customer_email })).id;
-    const price = session.line_items[0].price_data.unit_amount;
+    const price = session.amount_total / 100;
     await Booking.create({ tour, user, price });
 };
 
 exports.webhookCheckout = (req, res, next) => {
-    const signature = req.headers['stripe-signature'];
+    const signature = req.get('stripe-signature');
 
     let event;
     try {
@@ -74,7 +74,7 @@ exports.webhookCheckout = (req, res, next) => {
             process.env.STRIPE_WEBHOOK_SECRET
         );
     } catch (err) {
-        return res.status(400).send(`Webhook error:$ ${err.message}`);
+        return res.status(400).send(`Webhook error: ${err.message}`);
     }
 
     if (event.type === 'checkout.session.completed') {
