930d5434bdfdea22c6921f50782379d337a5b71b
souvikkoley9674 <113795422+souvikkoley9674@users.noreply.github.com>
2024-02-29 21:06:58 +0530

    Successful Booking Alert Show

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,10 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
-import { login, logout } from './login'
+import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alert';
+
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
@@ -70,3 +72,8 @@ if (bookBtn) {
         bookTour(tourId);
     });
 }
+
+const alertMessage = document.querySelector(body).dataset.alert;
+if (alertMessage) {
+    showAlert('success', alertMessage, 20);
+};
